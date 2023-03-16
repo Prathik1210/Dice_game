9ea62885db5a56622e9f0824177b5a65d6b8f6fe
Prathik1210 <112953449+Prathik1210@users.noreply.github.com>
2023-03-16 12:33:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var randomNumber2 = Math.random();
 randomNumber2 *=6;
 randomNumber2 = Math.floor(randomNumber2)+1;
 
-document.querySelector(".img1").setAttribute("src", "images/dice"+randomNumber1+".png");
-document.querySelector(".img2").setAttribute("src", "images/dice"+randomNumber2+".png");
+document.querySelector(".img1").setAttribute("src", "dice"+randomNumber1+".png");
+document.querySelector(".img2").setAttribute("src", "dice"+randomNumber2+".png");
 
 if (randomNumber1>randomNumber2){
   document.querySelector('h1').innerHTML = "Player1 Won!";
